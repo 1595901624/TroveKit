4ccd75fb9a87ecce7f9e93ecb92e879c11a9b683
luhaoyu <haoyu3@163.com>
2025-12-20 23:51:15 +0800

    remove unused imports and CSS file for a cleaner codebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
-import { Button } from "@heroui/react";
-
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className="app">
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
